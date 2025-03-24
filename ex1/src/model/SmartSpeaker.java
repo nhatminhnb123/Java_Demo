2708a1afd96556565bbc07ca21dc4acdd25d754b
DESKTOP-6G246L2\Minh <nhatminhnb123@gmail.com>
2025-03-24 16:44:01 +0700

    .

--- ex1/src/model/SmartSpeaker.java
+++ ex1/src/model/SmartSpeaker.java
@@ -1,4 +1,17 @@
 package model;
 
 public class SmartSpeaker extends SmartDevice implements Connectable{
+    public SmartSpeaker(String deviceName, boolean isOn) {
+        super(deviceName, isOn);
+    }
+
+    @Override
+    public void connectToWifi(String wifiAddress) {
+
+    }
+
+    @Override
+    void performFunction() {
+
+    }
 }
